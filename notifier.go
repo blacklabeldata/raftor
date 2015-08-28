1df6eddd1b538536435d60ae39fb2bf8533038c7
eliquious <max.franks@synapse-wireless.com>
2015-08-28 17:54:48 -0500

    Add ClusterChangeNotifier

---
+++ notifier.go
@@ -0,0 +1,8 @@
+package raftor
+
+// ClusterChangeNotifier notifies the receiver of the cluster change.
+type ClusterChangeNotifier interface {
+
+	// NotifyChange sends ClusterChangeEvents over the given channel when a node joins, leaves or is updated in the cluster.
+	NotifyChange() <-chan ClusterChangeEvent
+}
