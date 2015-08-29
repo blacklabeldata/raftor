8fb93d67c13265437003a8794ad4c54c0d93f284
eliquious <max.franks@synapse-wireless.com>
2015-08-28 21:12:06 -0500

    Add Stop method to ClusterChangeNotifier

--- notifier.go
+++ notifier.go
@@ -5,4 +5,7 @@ type ClusterChangeNotifier interface {
 
 	// NotifyChange sends ClusterChangeEvents over the given channel when a node joins, leaves or is updated in the cluster.
 	NotifyChange() <-chan ClusterChangeEvent
+
+	// Stop stops the notifier from sending out any more notifications.
+	Stop()
 }
